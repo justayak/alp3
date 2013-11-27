9c5f3b0389051d54b216639a28049dd497f2c54c
jutanke <jutanke@googlemail.com>
2013-11-27 11:28:47 +0100

    init

---
+++ src/aufgabe07/main.java
@@ -0,0 +1,16 @@
+package aufgabe07;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Julian
+ * Date: 27.11.13
+ * Time: 11:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+
+    public static void main(String[]args){
+        System.out.println("hiu");
+    }
+
+}

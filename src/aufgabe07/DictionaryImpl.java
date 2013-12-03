4767226ab3d1bb9f3c81d11dbaabbc4b247de490
justayak <justayak@googlemail.com>
2013-12-04 00:20:33 +0100

    aufgabe 3 stff

--- src/aufgabe07/DictionaryImpl.java
+++ src/aufgabe07/DictionaryImpl.java
@@ -25,41 +25,19 @@ public class DictionaryImpl<M> implements OrderedDictionary<String, M> {
 
     }
 
+    private enum NodeType {
+        Root,
+        Gateway,
+        Value
+    }
+
+
     private class Node{
         public M Value;
-        public final char Key;
-        public final int Position;
-        private Node[] children;
         public Node Left;
-        public Node right;
-        public boolean isLeaf(){
-            return this.Value != null;
-        }
-        public Node(char key){
-            this.Key = key;
-            this.Position = key - MIN_READABLE_CHAR;
-            this.children = (Node[]) new Object[(MAX_READABLE_CHAR - MIN_READABLE_CHAR)];
-        }
-        public M get(String key){
-            return this.get(key, 0);
-        }
-        private M get(String key, int index){
-            if (key.length() <= index) return null;
-            Node m = this.children[key.charAt(index) - MIN_READABLE_CHAR];
-            if (m == null) return null;
-            return m.get(key, index + 1);
-        }
-        public void add(String key, M value){
-            this.add(key, 0, value);
-        }
-        private void add(String key, int index, M value){
-            if (key.length() == index - 1){
-                this.Value = value;
-            }else if(key.length() > index) {
-
-            }
-            throw new RuntimeException("joa ne, sollt nicht passiern..");
-        }
+        public Node Right;
+        private Node[] children;
 
     }
+
 }
